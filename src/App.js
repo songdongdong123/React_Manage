d368e1c8ed5e84222debf0b18eb16a67d0f09e05
ethan <172529131@qq.com>
2018-09-18 21:42:49 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Button } from 'antd';
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Button>xxxxx</Button>
+      </div>
+    )
+  }
+}
+
+export default App;
