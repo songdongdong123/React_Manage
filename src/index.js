1665d7080ecaf3293a655191be857b3b83abae25
ethan <172529131@qq.com>
2018-09-19 15:47:40 +0800

    add redux

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import stroe from './redux';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={stroe}>
+    <App />
+  </Provider>
+  , document.getElementById('root'));
