05844f231f811a0ff43fe31653fb91ab73eb004d
Your Name <you@example.com>
2018-09-23 17:14:09 +0800

    add route config

---
+++ src/router/routeConfig/routeMap.js
@@ -0,0 +1,28 @@
+import loadComponent from './asyncLoadComponent';
+const _import_views = file => loadComponent(() => import(`../../pages/${file}`));
+export const routeLayOutMap = [
+  {
+    path: '/',
+    name: 'home',
+    exact: true,
+    component:_import_views('Home')
+  },{
+    path: '/ui/buttons',
+    name: 'buttons',
+    exact: true,
+    component:_import_views('UI/button')
+  },{
+    path: '/404',
+    name: 'nomatch',
+    exact: true,
+    component:_import_views('NoMatch')
+  }
+];
+
+export const routeNoLayOutMap = [
+  {
+    path: '/login',
+    name: 'login',
+    component:_import_views('Login')
+  }
+]
