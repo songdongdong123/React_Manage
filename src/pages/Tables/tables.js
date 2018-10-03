9635a2def77c19292a0cf0d0b0ad85a42f7d6558
Your Name <you@example.com>
2018-10-03 12:52:50 +0800

    add tables

--- src/pages/Tables/tables.js
+++ src/pages/Tables/tables.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Card, Table, Divider, Tag  } from 'antd'
-// import './base.styl'
+import '../UI/base.styl'
+const { Column, ColumnGroup } = Table;
 const columns = [{
   title: 'Name',
   dataIndex: 'name',
@@ -54,6 +55,29 @@ const data = [{
   address: 'Sidney No. 1 Lake Park',
   tags: ['cool', 'teacher'],
 }];
+const datas = [{
+  key: '1',
+  firstName: 'John',
+  lastName: 'Brown',
+  age: 32,
+  address: 'New York No. 1 Lake Park',
+  tags: ['nice', 'developer'],
+}, {
+  key: '2',
+  firstName: 'Jim',
+  lastName: 'Green',
+  age: 42,
+  address: 'London No. 1 Lake Park',
+  tags: ['loser'],
+}, {
+  key: '3',
+  firstName: 'Joe',
+  lastName: 'Black',
+  age: 32,
+  address: 'Sidney No. 1 Lake Park',
+  tags: ['cool', 'teacher'],
+}];
+
 export default class Modals extends React.Component {
   constructor(props) {
     super(props);
@@ -67,8 +91,52 @@ export default class Modals extends React.Component {
         <Card title="基本用法" className="Card">
           <Table columns={columns} dataSource={data} />
         </Card>
-        <Card title="禁用" className="Card">
-          
+        <Card title="使用 JSX 风格的 API（2.5.0 以后引入）" className="Card">
+          <Table dataSource={datas}>
+            <ColumnGroup title="Name">
+              <Column
+                title="First Name"
+                dataIndex="firstName"
+                key="firstName"
+              />
+              <Column
+                title="Last Name"
+                dataIndex="lastName"
+                key="lastName"
+              />
+            </ColumnGroup>
+            <Column
+              title="Age"
+              dataIndex="age"
+              key="age"
+            />
+            <Column
+              title="Address"
+              dataIndex="address"
+              key="address"
+            />
+            <Column
+              title="Tags"
+              dataIndex="tags"
+              key="tags"
+              render={tags => (
+                <span>
+                  {tags.map(tag => <Tag color="blue" key={tag}>{tag}</Tag>)}
+                </span>
+              )}
+            />
+            <Column
+              title="Action"
+              key="action"
+              render={(text, record) => (
+                <span>
+                  <a href="javascript:;">Invite {record.lastName}</a>
+                  <Divider type="vertical" />
+                  <a href="javascript:;">Delete</a>
+                </span>
+              )}
+            />
+          </Table>
         </Card>
         <Card title="图标" className="Card">
           
